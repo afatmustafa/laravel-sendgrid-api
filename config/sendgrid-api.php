0859b9e6f997984dc5c53cf6d934bc34f8cd6af6
Mustafa Afat <59372593+afatmustafa@users.noreply.github.com>
2022-06-15 10:13:04 +0300

    Update sendgrid-api.php

--- config/sendgrid-api.php
+++ config/sendgrid-api.php
@@ -3,11 +3,11 @@
 return [
     'api_key' => env('SENDGRID_API_KEY'),
     'from' => [
-        'email' => env('SENDGRID_FROM_EMAIL', 'no-reply@verymoney.com'),
-        'name' => env('SENDGRID_FROM_NAME', 'Verymoney'),
+        'email' => env('SENDGRID_FROM_EMAIL'),
+        'name' => env('SENDGRID_FROM_NAME'),
     ],
     'reply_to' => [
-        'email' => env('SENDGRID_REPLY_TO_EMAIL', 'info@verymoney.com'),
-        'name' => env('SENDGRID_REPLY_TO_NAME', 'Verymoney'),
+        'email' => env('SENDGRID_REPLY_TO_EMAIL'),
+        'name' => env('SENDGRID_REPLY_TO_NAME'),
     ],
 ];
